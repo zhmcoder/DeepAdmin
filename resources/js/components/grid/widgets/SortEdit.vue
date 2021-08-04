58636e1f7f2afa09e5f704a65060214ff50e4f10
zhaomeng <zhmzhaomeng@hotmail.com>
2021-08-04 15:54:15 +0800

    增加:表格统计汇总显示

--- resources/js/components/grid/widgets/SortEdit.vue
+++ resources/js/components/grid/widgets/SortEdit.vue
@@ -1,67 +1,72 @@
 <template slot-scope="scope">
-    <div class="rank">
-        <input type="text" v-model="sort_value" @blur="sort_change(1)" :style="{ width: attrs.width + 'px'}"/>
-    </div>
+  <div class="rank">
+    <input type="text" v-model="sort_value" @blur="sort_change(1)" :style="{ width: attrs.width + 'px'}"/>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            attrs: Object,
-            row: Object,
-            column_value: {
-                default: null
-            },
-            value: {
-                default: null
-            }
-        },
-        data() {
-            return {
-                sort_value: this.value,
-                old_sort_value: this.value,
-            };
-        },
-        mounted() {
-        },
-        methods: {
-            onRequest(uri) {
-                this.loading = true;
-                this.$http
-                    .get(uri)
-                    .then(res => {
-                        if (res.code == 200) {
+	export default {
+		props: {
+			attrs: Object,
+			row: Object,
+			column_value: {
+				default: null
+			},
+			value: {
+				default: null
+			}
+		},
+		data() {
+			return {
+				sort_value: this.value,
+				old_sort_value: this.value,
+			};
+		},
+		mounted() {
+		},
+		methods: {
+			onRequest(uri) {
+				console.log(this.row)
+				this.loading = true;
+				this.$http
+					.get(uri)
+					.then(res => {
+						if (res.code == 200) {
+							if(this.attrs.showSummary){
+								this.row[this.attrs.field] = this.sort_value;
+              }
 
-                        }
-                    })
-                    .finally(() => {
-                        this.loading = false;
-                    });
-            },
-            sort_change(index) {
-                console.log("index = " + index);
-                console.log(this.sort_value + " sort value");
-                console.log(this.old_sort_value + " old_sort_value");
-                console.log(this.old_sort_value + " old_sort_value");
-                if (this.attrs.action != null && this.old_sort_value != this.sort_value) {
-                    this.onRequest(this.attrs.action + '&id=' +
-                        this.row.id + "&sort_value=" + this.sort_value);
-                }
-            }
-        }
-    }
+							console.log(this.row)
+						}
+					})
+					.finally(() => {
+						this.loading = false;
+					});
+			},
+			sort_change(index) {
+				console.log("index = " + index);
+				console.log(this.sort_value + " sort value");
+				console.log(this.old_sort_value + " old_sort_value");
+				console.log(this.old_sort_value + " old_sort_value");
+				if (this.attrs.action != null && this.old_sort_value != this.sort_value) {
+					this.onRequest(this.attrs.action + '&id=' +
+						this.row.id + "&sort_value=" + this.sort_value);
+				}
+			}
+		}
+	}
 </script>
 <style lang="scss" scoped>
-    .rank {
-        input {
-            width: 100%;
-            display: inline-block;
-            border: 1px solid #DCDFE6;
-            outline: none;
-            padding: 5px;
-            color: #606266;
-        }
+  .rank {
+    input {
+      width: 100%;
+      display: inline-block;
+      border: 1px solid #DCDFE6;
+      outline: none;
+      padding: 5px;
+      color: #606266;
     }
+  }
 
 </style>
 
--- src/Components/Grid/SortEdit.php
+++ src/Components/Grid/SortEdit.php
@@ -11,21 +11,57 @@ class SortEdit extends Component
     protected $componentName = "SortEdit";
     protected $action = null;
     protected $width = 60;
+    protected $field = 'sort';
+    protected $showSummary = false;
 
     public static function make($value = null)
     {
         return new SortEdit($value);
     }
 
+    /**
+     * 请求地址
+     * @param $action
+     * @return $this
+     */
     public function action($action)
     {
         $this->action = $action;
         return $this;
     }
 
+    /**
+     * 宽度
+     * @param $width
+     * @return $this
+     */
     public function width($width)
     {
         $this->width = $width;
+        return $this;
+    }
+
+    /**
+     * 编辑对应的字段名称
+     *
+     * @param $filed
+     * @return $this
+     */
+    public function field($field)
+    {
+        $this->field = $field;
+        return $this;
+    }
+
+    /**
+     * 编辑后是否进行表格汇总更新
+     *
+     * @param $showSummary
+     * @return $this
+     */
+    public function showSummary($showSummary)
+    {
+        $this->showSummary = $showSummary;
         return $this;
     }
 
