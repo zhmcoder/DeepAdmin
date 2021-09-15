64e2054eaa2716336a94971294949bae33835f7f
fanzewei <fanzewei@yongche.com>
2021-09-15 15:31:47 +0800

    序号默认居中

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -206,7 +206,7 @@ class ContentController extends AdminController
 
         // 是否显示ID字段
         if (Schema::hasColumn($this->entity->table_name, 'id')) {
-            $grid->column('id', '序号')->sortable()->width(80);
+            $grid->column('id', '序号')->sortable()->width(80)->align('center');
         }
 
         $entityField = EntityField::query()->where('entity_id', $this->entityId)
