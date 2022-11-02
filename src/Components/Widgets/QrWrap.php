69db8740b881a0d67ad14416441323ea29edbb14
fanzewei <fanzewei@yongche.com>
2022-11-02 17:41:41 +0800

    二维码功能

--- src/Components/Widgets/QrWrap.php
+++ src/Components/Widgets/QrWrap.php
@@ -7,9 +7,27 @@ use Andruby\DeepAdmin\Components\Component;
 class QrWrap extends Component
 {
     protected $componentName = 'QrWrap';
+    protected $href = '';
+    protected $src = '';
 
     public static function make()
     {
         return new QrWrap();
+    }
+
+    /**
+     * 链接地址
+     */
+    public function setHref($href = '')
+    {
+        $this->href = $href;
+    }
+
+    /**
+     * 图片地址
+     */
+    public function setSrc($src = '')
+    {
+        $this->src = $src;
     }
 }
