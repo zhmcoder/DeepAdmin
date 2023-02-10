e2b2f8d3ec6d322aceb85c33920e842696d17418
范泽伟 <fans_wei@163.com>
2023-02-10 17:55:27 +0800

    修改密码接口

---
+++ src/Validates/AdminUserValidate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Andruby\DeepAdmin\Validates;
+
+use Andruby\Login\Validates\Validate;
+
+class AdminUserValidate extends Validate
+{
+    public function resetPwd($request_data)
+    {
+        $rules = [
+            'password_confirm' => 'required|same:password|min:6',
+            'password' => 'required|min:6',
+        ];
+        $message = [
+            'password.required' => '密码不能为空',
+            'password.min' => '密码长度至少为6个字符',
+            'password_confirm.required' => '确认密码不能为空',
+            'password_confirm.same' => '密码与确认密码不一致',
+            'password_confirm.min' => '密码长度至少为6个字符',
+        ];
+        return $this->validate($request_data, $rules, $message);
+    }
+}
