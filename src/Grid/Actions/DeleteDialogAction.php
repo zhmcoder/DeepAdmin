e0c712c4d0fc31700e5e097073afc45c3e00cdf5
范泽伟 <fans_wei@163.com>
2024-03-22 16:13:15 +0800

    搜索重置按钮

--- src/Grid/Actions/DeleteDialogAction.php
+++ src/Grid/Actions/DeleteDialogAction.php
@@ -13,4 +13,43 @@ class DeleteDialogAction extends BaseRowAction
     protected $content = "删除";
 
     protected $message = "确定要删除此内容？";
+
+    protected $title = '删除提示';
+
+    protected $cancel = '取消';
+
+    protected $confirm = '确认';
+
+    /**
+     * title 名称
+     * @param mixed $title
+     * @return $this
+     */
+    public function title($title): DeleteDialogAction
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * cancel 名称
+     * @param mixed $cancel
+     * @return $this
+     */
+    public function cancel($cancel): DeleteDialogAction
+    {
+        $this->cancel = $cancel;
+        return $this;
+    }
+
+    /**
+     * confirm 名称
+     * @param mixed $confirm
+     * @return $this
+     */
+    public function confirm($confirm): DeleteDialogAction
+    {
+        $this->confirm = $confirm;
+        return $this;
+    }
 }
