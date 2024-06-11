3f0e0fbcc8d6b364e3f8faecb86021c2cf40134a
wangshaobo <1158799115@qq.com>
2024-06-11 10:29:20 +0800

    update config

--- config/deep_admin.php
+++ config/deep_admin.php
@@ -170,6 +170,9 @@ return [
         'subfield' => true,
         'preview' => true
     ],
+    'baidu_editor' => [
+        'js_base_path' => env('BAIDU_EDITOR_JS_BASE_PATH', 'https://image.bioss.zdapk.cn/UEditor/'),
+    ],
     // 数据库表字段类型 参考：https://laravel.com/docs/5.5/migrations#columns
     'db_table_field_type' => [
         'char' => '定长字符串（char）',
