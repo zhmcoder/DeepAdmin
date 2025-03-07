2fa7a71ca2307aebd9153a4a2ae9d14d832327f8
zhaomeng <zhmzhaomeng@hotmail.com>
2025-03-07 12:48:32 +0800

    datetime:添加摄氏

--- src/Models/Content.php
+++ src/Models/Content.php
@@ -34,8 +34,8 @@ class Content extends Model
     protected $hidden = ['created_at', 'updated_at'];
 
     protected $casts = [
-        'created_at' => "Y-m-d H:i:s",
-        'updated_at' => "Y-m-d H:i:s",
+        'created_at' => "datetime:Y-m-d H:i:s",
+        'updated_at' => "datetime:Y-m-d H:i:s",
     ];
 
     public static $listField = [
